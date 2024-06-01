37c34741e05d8c42dd1b26aa92e5e149e627dcc3
Izotov Ivan <che1-you@.ru>
2024-06-02 00:37:02 +1000

    Initial commit

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,11 @@
 import os
-from aiogram import F, Router
+from aiogram import F, Router, Bot
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.enums.parse_mode import ParseMode
 from dotenv import load_dotenv
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 import app.keyboards as kb
 
 load_dotenv()
@@ -17,30 +19,46 @@ TEACHER_ID = int(os.getenv('TEACHER_ID'))
 
 @admin.message(Command('admin'))
 async def cmd_admin(message: Message):
-	if ADMIN_ID == message.from_user.id:
-		await message.answer('Вы в админ панели!')
-	else:
-		await message.answer('В доступе отказано!')
+    if ADMIN_ID == message.from_user.id:
+        await message.answer('Вы в админ панели!', reply_markup=kb.admin_panel())
+    else:
+        await message.answer('В доступе отказано!')
 
 
 @admin.message(F.text == 'Личный кабинет')
 async def per_acc_adm(message: Message):
-	if ADMIN_ID == message.from_user.id:
-		await message.answer('Вы в админ панели',
-							 reply_markup=kb.admin_panel())
-	elif STUDENT_ID == message.from_user.id:
-		await message.answer(f'Добро пожаловать,'
-							 f' {message.from_user.full_name}, студент!',
-							 reply_markup=kb.std_panel())
-	elif TEACHER_ID == message.from_user.id:
-		await message.answer(f'Добро пожаловать преподаватель,'
-							 f' <b>{message.from_user.full_name}!</b>',
-							 parse_mode=ParseMode('HTML'),
-							 reply_markup=kb.teacher_panel())
-	else:
-		await message.answer('Вы отсутствуете в списках ЭнергоБота')
+    if ADMIN_ID == message.from_user.id:
+        await message.answer('Вы в админ панели',
+                             reply_markup=kb.admin_panel())
+    elif STUDENT_ID == message.from_user.id:
+        await message.answer(f'Добро пожаловать,'
+                             f' {message.from_user.full_name}, студент!',
+                             reply_markup=kb.std_panel())
+    elif TEACHER_ID == message.from_user.id:
+        await message.answer(f'Добро пожаловать преподаватель,'
+                             f' <b>{message.from_user.full_name}!</b>',
+                             parse_mode=ParseMode('HTML'),
+                             reply_markup=kb.teacher_panel())
+    else:
+        await message.answer('Вы отсутствуете в списках ЭнергоБота')
 
 
-
-
-
+# class Broadcast(StatesGroup):
+#     wait_broadcast_message = State()
+#
+#
+# @admin.message(F.text == 'Сделать рассылку')
+# async def cmd_broadcast(message: Message, state: FSMContext):
+#     await state.update_data(wait_broadcast_message=message.text)
+#     await state.set_state(Broadcast.wait_broadcast_message)
+#     await message.answer('Напишите сообщение для рассылки - оно будет отправлено '
+#                          'всем пользователям')
+#
+#
+# @admin.message(Broadcast.wait_broadcast_message)
+# async def send_broadcast(message: Message, state: FSMContext):
+#     if message.text.startswith('Сделать рассылку'):
+#         return
+#     await send_broadcast_all(message)
+#     await message.answer('Сообщение успешно отправлено')
+#     await state.clear()
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,69 +1,69 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
-						   InlineKeyboardMarkup, InlineKeyboardButton)
+                           InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
-	[KeyboardButton(text='Личный кабинет')],
-	[KeyboardButton(text='Узнать о боте'),
-	 KeyboardButton(text='Сайт колледжа')],
-	[KeyboardButton(text='Новости колледжа')]
+    [KeyboardButton(text='Личный кабинет')],
+    [KeyboardButton(text='Узнать о боте'),
+     KeyboardButton(text='Сайт колледжа')],
+    [KeyboardButton(text='Новости колледжа')]
 ],
-	resize_keyboard=True,
-	input_field_placeholder='Выберите пункт меню')
+    resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню')
 
 site = InlineKeyboardMarkup(inline_keyboard=[
-	[InlineKeyboardButton(text='Перейти на сайт колледжа', url='https://www.ekvl.ru/')]
+    [InlineKeyboardButton(text='Перейти на сайт колледжа', url='https://www.ekvl.ru/')]
 ])
 
 group = InlineKeyboardMarkup(inline_keyboard=[
-	[InlineKeyboardButton(text='Перейти в канал колледжа:', url='https://t.me/EnergyCollege')]
+    [InlineKeyboardButton(text='Перейти в канал колледжа:', url='https://t.me/EnergyCollege')]
 ])
 
 data = ('Показать пользователей', 'Добавить пользователя',
-		'Удалить пользователя', 'Сделать рассылку', 'Назад')
+        'Удалить пользователя', 'Сделать рассылку', 'Назад')
 
 answer = InlineKeyboardMarkup(inline_keyboard=[
-	[InlineKeyboardButton(text='Ответить', callback_data='answer')]
+    [InlineKeyboardButton(text='Ответить', callback_data='answer')]
 ])
 
 st_answer = InlineKeyboardMarkup(inline_keyboard=[
-	[InlineKeyboardButton(text='Ответить', callback_data='st_answer')]
+    [InlineKeyboardButton(text='Ответить', callback_data='st_answer')]
 ])
 
 
 def admin_panel():
-	keyboard = ReplyKeyboardBuilder()
-	for admin_panel in data:
-		keyboard.add(KeyboardButton(text=admin_panel))
-	return keyboard.adjust(2).as_markup(resize_keyboard=True)
+    keyboard = ReplyKeyboardBuilder()
+    for admin_panel in data:
+        keyboard.add(KeyboardButton(text=admin_panel))
+    return keyboard.adjust(2).as_markup(resize_keyboard=True)
 
 
 data_std = ('Показать тесты', 'Связь с преподавателем',
-			'Учебные материалы', 'Расписание пар',
-			'Меню столовой', 'Назад')
+            'Учебные материалы', 'Расписание пар',
+            'Меню столовой', 'Назад')
 
 
 def std_panel():
-	keyboard = ReplyKeyboardBuilder()
-	for std_panel in data_std:
-		keyboard.add(KeyboardButton(text=std_panel))
-	return keyboard.adjust(2).as_markup(resize_keyboard=True)
+    keyboard = ReplyKeyboardBuilder()
+    for std_panel in data_std:
+        keyboard.add(KeyboardButton(text=std_panel))
+    return keyboard.adjust(2).as_markup(resize_keyboard=True)
 
 
 key_teacher = ('Создать тест', 'Расписание', 'Рассылка группе', 'Связь с админом')
 
 
 def teacher_panel():
-	keyboard = ReplyKeyboardBuilder()
-	for teacher_panel in key_teacher:
-		keyboard.add(KeyboardButton(text=teacher_panel))
-	return keyboard.adjust(2).as_markup(resize_keyboard=True)
+    keyboard = ReplyKeyboardBuilder()
+    for teacher_panel in key_teacher:
+        keyboard.add(KeyboardButton(text=teacher_panel))
+    return keyboard.adjust(2).as_markup(resize_keyboard=True)
 
 
 docs_panel = InlineKeyboardMarkup(inline_keyboard=[
-	[InlineKeyboardButton(text='Реферат', callback_data='Ref')],
-	[InlineKeyboardButton(text='Курсовая', callback_data='Kur')],
-	[InlineKeyboardButton(text='Отчет по практике', callback_data='Otc')],
-	[InlineKeyboardButton(text='Дипломная работа', callback_data='Dip')],
-	[InlineKeyboardButton(text='Оформление документа', callback_data='Gos')]
+    [InlineKeyboardButton(text='Реферат', callback_data='Ref')],
+    [InlineKeyboardButton(text='Курсовая', callback_data='Kur')],
+    [InlineKeyboardButton(text='Отчет по практике', callback_data='Otc')],
+    [InlineKeyboardButton(text='Дипломная работа', callback_data='Dip')],
+    [InlineKeyboardButton(text='Оформление документа', callback_data='Gos')]
 ])
