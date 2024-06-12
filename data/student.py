fc7542c82747daf860a8b9e3f2e1e659917dc656
Izotov Ivan <che1-you@.ru>
2024-06-13 00:43:49 +1000

    Меню столовой

--- data/student.py
+++ data/student.py
@@ -20,71 +20,75 @@ TEACHER_ID = int(os.getenv('TEACHER_ID'))
 
 @std.message(F.text == 'Показать тесты')
 async def show_test(message: Message):
-	await message.answer('На данный момент тестов для прохождения нет')
+    await message.answer('На данный момент тестов для прохождения нет')
 
 
 class TeacherContact(StatesGroup):
-	waiting_for_message = State()
-	waiting_for_response1 = State()
+    waiting_for_message = State()
+    waiting_for_response1 = State()
 
 
 async def send_message_to_teacher(message: Message):
-	await bot.send_message(TEACHER_ID, f'<b>Сообщение от студента,'
-									f' {message.from_user.full_name}\n</b>'
-									f' {message.text}',
-						   			reply_markup=kb.answer,
-						   			parse_mode=ParseMode.HTML)
+    await bot.send_message(TEACHER_ID, f'<b>Сообщение от студента,'
+                                       f' {message.from_user.full_name}\n</b>'
+                                       f' {message.text}',
+                           reply_markup=kb.answer,
+                           parse_mode=ParseMode.HTML)
 
 
 @std.callback_query(F.data.startswith('st_answer'))
 async def answer_to_teacher(callback: CallbackQuery, state: FSMContext):
-	if callback.data.startswith('st_answer'):
-		await state.update_data(waiting_for_response1=callback.data)
-		await state.set_state(TeacherContact.waiting_for_response1)
-		await callback.message.answer('Напишите ответ преподавателю')
-		await callback.answer('')
-	else:
-		await callback.answer('Нажмите на кнопку "Ответить"')
+    if callback.data.startswith('st_answer'):
+        await state.update_data(waiting_for_response1=callback.data)
+        await state.set_state(TeacherContact.waiting_for_response1)
+        await callback.message.answer('Напишите ответ преподавателю')
+        await callback.answer('')
+    else:
+        await callback.answer('Нажмите на кнопку "Ответить"')
 
 
 @std.message(TeacherContact.waiting_for_response1)
 async def student_response(message: Message, state: FSMContext):
-	await send_message_to_teacher(message)
-	await message.answer('Ответ успешно отправлен')
-	await state.clear()
+    await send_message_to_teacher(message)
+    await message.answer('Ответ успешно отправлен')
+    await state.clear()
 
 
 @std.message(F.text == 'Связь с преподавателем')
 async def teacher_contact(message: Message, state: FSMContext):
-	await state.update_data(waiting_for_message=message.text)
-	await state.set_state(TeacherContact.waiting_for_message)
-	await message.answer('Напишите сообщение преподавателю')
+    await state.update_data(waiting_for_message=message.text)
+    await state.set_state(TeacherContact.waiting_for_message)
+    await message.answer('Напишите сообщение преподавателю')
 
 
 @std.message(TeacherContact.waiting_for_message)
 async def teacher_connect_text(message: Message, state: FSMContext):
-	if message.text.startswith('Связь с преподавателем'):
-		return
-	await send_message_to_teacher(message)
-	await message.answer('Сообщение успешно отправлено')
-	await state.clear()
+    if message.text.startswith('Связь с преподавателем'):
+        return
+    await send_message_to_teacher(message)
+    await message.answer('Сообщение успешно отправлено')
+    await state.clear()
 
 
 @std.message(F.text == 'Учебные материалы')
 async def cmd_docs(message: Message):
-	await message.answer('Учебные документы и шаблоны', reply_markup=kb.docs_panel)
+    await message.answer('Учебные документы и шаблоны', reply_markup=kb.docs_panel)
 
 
 @std.message(F.text == 'Расписание пар')
 async def text_week_plan(message: Message):
-	await message.answer('<b>Расписание на понедельник:</b>\n'
-								  '1) Математика - Николай И.Н. 301 каб \n'
-								  '2) Русский язык - Иванова А.М. 105 каб.\n'
-								  '3) Информатика - Лебедев Н.О. 224 каб.\n'
-								  '4) Физукультура - Елисеев А.П. 111 каб.',
-								  parse_mode=ParseMode.HTML)
+    await message.answer('<b>Расписание на понедельник:</b>\n'
+                         '1) Математика - Николай И.Н. 301 каб \n'
+                         '2) Русский язык - Иванова А.М. 105 каб.\n'
+                         '3) Информатика - Лебедев Н.О. 224 каб.\n'
+                         '4) Физкультура - Елисеев А.П. 111 каб.',
+                         parse_mode=ParseMode.HTML)
 
 
-
-
-
+@std.message(F.text == 'Меню столовой')
+async def text_menu(message: Message):
+    await message.answer('<b>Меню на сегодня:\n</b>'
+                         '1)Борщ со сметаной 🍜\n'
+                         '2)Макороны с куриной котлетой 🍝\n'
+                         '3)Компот 🧃',
+                         parse_mode=ParseMode.HTML)
