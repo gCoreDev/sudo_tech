a55d790e482667fbc0c956b7de66282ec5067d9a
gCoreDev <che1-you@ya.ru>
2024-06-30 13:56:48 +1000

    create_data_base.py & clears & rights imports

--- handlers/student.py
+++ handlers/student.py
@@ -25,10 +25,7 @@ async def show_test(message: Message):
         [InlineKeyboardButton(text=test_name, callback_data=f"start_test_{test_id}")]
         for test_id, test_name in tests
     ])
-    conn_tests.close()
     await message.answer("Доступные тесты:", reply_markup=keyboard)
-
-
 
 
 @std.callback_query(lambda c: c.data.startswith("start_test_"))
